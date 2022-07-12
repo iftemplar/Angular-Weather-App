65b625bf0348436bc68cb57150286fb7b8ddb933
Mykola Samila <samila.mykola@gmail.com>
2022-07-12 17:41:01 +0200

    Created Weather Service, used it for Demo request (output in console)

--- src/app/weather-search/weather-search.component.ts
+++ src/app/weather-search/weather-search.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../weather.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-weather-search',
   templateUrl: './weather-search.component.html',
-  styleUrls: ['./weather-search.component.scss']
+  styleUrls: ['./weather-search.component.scss'],
 })
 export class WeatherSearchComponent implements OnInit {
+  weatherSubscription?: Subscription;
 
-  constructor() { }
+  constructor(private weatherService: WeatherService) {}
 
   ngOnInit(): void {
+    this.weatherSubscription = this.weatherService.fetchWeather().subscribe((data) => {
+      console.log(data);
+    });
   }
-
 }
