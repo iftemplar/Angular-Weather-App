2de5a280091a0bc72718d19bcf5006c8b0a0a0b6
Mykola Samila <samila.mykola@gmail.com>
2022-07-12 13:40:47 +0200

    Added prettier, Bootstrap. Created Weather Search component. Added basic UI.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WeatherSearchComponent } from './weather-search/weather-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherSearchComponent
   ],
   imports: [
     BrowserModule
